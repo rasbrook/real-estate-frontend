f496124c92181f6803c0256c8b41068dbe362add
rasbrook <brookbra17@gmail.com>
2024-11-25 20:46:00 +0300

    hopfully last commit before

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'; 
+import { nodePolyfills } from 'vite-plugin-node-polyfills'; 
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills()],
   server: {
     proxy: {
       '/api': {
